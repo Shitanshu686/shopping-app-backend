4d6b2698a01259974303a06424aefa45c568382f
Shitanshu Jha <shitanshujha40@gmail.com~>
2026-07-20 17:53:30 +0530

    Initial Spring Boot project

---
+++ src/test/java/com/shitanshu/shopping/ShoppingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shitanshu.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
